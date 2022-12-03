d3a70f61bf1cb95d6f3acb734b6ee9d88c589b1c
曹磊 <lcaotx@gmail.com>
2022-12-03 15:14:06 +0800

    集成 gin

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello 世界!")
+	r := gin.New()
+	r.Use(gin.Logger(), gin.Recovery())
+	r.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+	r.NoRoute(func(context *gin.Context) {
+		acceptString := context.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html"){
+			context.String(http.StatusNotFound,"页面返回404")
+		}else {
+			context.JSON(http.StatusNotFound,gin.H{
+				"error_code":404,
+				"error_message":"路由未定义",
+			})
+		}
+	})
+	r.Run(":8000")
 }
