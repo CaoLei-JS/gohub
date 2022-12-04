169f6d68427d94769bb61708cc173bbb7e4d3419
开发部/曹磊 <cao870@biox.org.cn>
2022-12-04 11:18:54 +0800

    手机是否注册接口

--- routes/api.go
+++ routes/api.go
@@ -2,16 +2,17 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"gohub/app/http/controllers/api/v1/auth"
 )
 
 func RegisterApiRoutes(r *gin.Engine) {
 	v1 := r.Group("/v1")
 	{
-		v1.GET("/", func(context *gin.Context) {
-			context.JSON(http.StatusOK, gin.H{
-				"Hello":"World!",
-			})
-		})
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
 	}
 }
